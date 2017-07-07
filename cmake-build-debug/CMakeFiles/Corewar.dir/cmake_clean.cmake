714424cedb0b99094618754326a841fb7f1be935
mponomar <beardings@gmail.com>
2017-07-07 16:50:57 +0300

    start corevare

---
+++ cmake-build-debug/CMakeFiles/Corewar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Corewar.dir/main.c.o"
+  "Corewar.pdb"
+  "Corewar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Corewar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
