74358667734c7ceee40731c44c145551da23ab23
beardings <beardings@gmail.com>
2017-09-05 20:45:39 +0300

    need validate cor file with player

--- vm/controller/get_players.c
+++ vm/controller/get_players.c
@@ -1,20 +1,28 @@
 #include "../includes/vm.h"
 
-/*t_players		*get_players(char **argv, int argc)
+void            check_cor_file(t_players **players, char *cor_file)
+{
+
+}
+
+// я могу принимать игроков и без n номера игрока, нужно нормально чекать
+t_players		*get_players(char **argv, int argc)
 {
     t_players *players;
     int i;
 
     i = 1;
+    players = create_players();
     while (argc > 0)
     {
         if (!(ft_strcmp(argv[i], "-n")))
         {
-
+            players->num = ft_atoi(argv[i + 1]);
+            check_cor_file(&players, argv[i + 2]);
             i += 2;
         }
         argc--;
         i++;
     }
 	return (players);
-}*/
+}
